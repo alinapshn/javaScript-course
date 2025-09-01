5a81bf40585540ed355d99f6d282421ce7c10f87
alina <alina@macbook-pro.local>
2025-09-01 21:33:50 +0300

    added lesson 11 (part 2)

--- homeworks/11-homework.js
+++ homeworks/11-homework.js
@@ -145,3 +145,102 @@ function countWords(words) {
 }
 
 console.log(countWords(["apple", "grape", "apple", "apple"]));
+
+// 11o
+let words = ["hello", "world", "search", "good"];
+let index = -1;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "search") {
+    index = i;
+  }
+}
+
+console.log(index);
+
+words = ["not", "found"];
+index = -1;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "search") {
+    index = i;
+  }
+}
+
+console.log(index);
+
+// 11p
+words = ["hello", "world", "search", "good", "search"];
+index = -1;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "search") {
+    index = i;
+    break;
+  }
+}
+console.log(index);
+
+// 11q
+function findIndex(array, word) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === word) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(findIndex(["green", "red", "blue", "red"], "red"));
+console.log(findIndex(["green", "red", "blue", "red"], "yellow"));
+
+// 11r, 11s, 11t, 11u
+function removeEgg(foods) {
+  let foodsCopy = foods.slice();
+  let reversedFoods = foods.reverse();
+
+  let result = [];
+  let eggsRemoved = 0;
+
+  for (let i = 0; i < reversedFoods.length; i++) {
+    if (reversedFoods[i] === "egg" && eggsRemoved < 2) {
+      eggsRemoved++;
+      continue;
+    }
+    result.push(reversedFoods[i]);
+  }
+  return result.reverse();
+}
+
+const foods = ["egg", "apple", "egg", "egg", "ham"];
+console.log(removeEgg(foods));
+console.log(foods);
+
+// 11v
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else {
+    console.log(i);
+  }
+}
+
+// 11w
+function unique(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    let word = array[i];
+    if (findIndex(result, word) === -1) {
+      result.push(word);
+    }
+  }
+  return result;
+}
+
+console.log(unique(["green", "red", "blue", "red"]));
+console.log(unique(["red", "green", "green", "red"]));
